3f159772188ce0c79cc7c00a6159d0255af918c1
Robson Salvador Franco <robsongit1@gmail.com>
2026-01-20 20:51:22 -0300

    Aula 3
    * Criação das entidades e relacionamentos (Model)

---
+++ src/main/java/com/RecHuman/RecHuman/entity/Candidato.java
@@ -0,0 +1,100 @@
+package com.RecHuman.RecHuman.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+public class Candidato implements Serializable {
+
+	private static final long serialVersionUID = 6629142382084665682L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(unique = true)
+	private String rg;
+	
+	@NotEmpty
+	private String nomeCandidato;
+	
+	@NotEmpty
+	private String email;
+	
+	@NotEmpty
+	private String endereco;
+	
+	@NotEmpty
+	private int idade;
+	
+	@ManyToOne
+	private Vaga vaga;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getNomeCandidato() {
+		return nomeCandidato;
+	}
+
+	public void setNomeCandidato(String nomeCandidato) {
+		this.nomeCandidato = nomeCandidato;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Vaga getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(Vaga vaga) {
+		this.vaga = vaga;
+	}
+	
+	
+	
+	
+
+}
